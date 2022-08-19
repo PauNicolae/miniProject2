e132b7de444a454826c1f810041c0f21faadd369
jlbbacs <josephlestebacs@gmail.com>
2022-08-19 19:07:25 +0800

    modify shop add to cart and payment

--- Item.js
+++ Item.js
@@ -1,52 +1,62 @@
 const params = new URLSearchParams(window.location.search);
-let product = params.get('product');
+let product = params.get("product");
 const url = `https://62f7c3d7ab9f1f8e8902fb45.mockapi.io/api/v1/exercise_items/${product}`;
-const subContainer = document.querySelector('.subContainer');
+const subContainer = document.querySelector(".subContainer");
 let data = [];
 let next = parseInt(product) + 1;
 let prev = parseInt(product) - 1;
 //user input should show data
 
-
 const fetchData = async () => {
-   let paulene = await fetch(url);
+  let paulene = await fetch(url);
 
-   if (paulene.status === 200) {
-      data = await paulene.json();    
-      displayData(data);
-      return false;
-   }
-   window.history.back()
+  if (paulene.status === 200) {
+    data = await paulene.json();
+    displayData(data);
+    return false;
+  }
+  window.history.back();
+};
+
+const options = function (id, objects, target) {
+  let opt = "<option selected value='n_a'>Select</option>";
+  for (const key in objects) {
+    if (objects.hasOwnProperty(key)) {
+      if (target == "size" && cart[id]) {
+        opt += `<option ${
+          cart[id].size == key ? "selected" : ""
+        } value="${key}">${objects[key]}</option>`;
+      } else if (target == "color" && cart[id]) {
+        opt += `<option ${
+          cart[id].color == key ? "selected" : ""
+        } value="${key}">${objects[key]}</option>`;
+      } else {
+        opt += `<option value="${key}">${objects[key]}</option>`;
+      }
+    }
+  }
+  return opt;
 };
 
 const displayData = (item) => {
-   let htmlString = '';
-     
-         // new inserted code
-        
-            htmlString += `
-            <div class="container">
+  let htmlString = "";
+
+  htmlString += `
+            <div class="container cart-item">
             <div class="row">
                <div class="col-md-5 pt-3">
-                  
                   <div class="card border shadow mt-5">
                      <img
                         src="${item.mainphoto}"
                         alt="noLimitSportsBra"
                         class="img-fluid"
                      />
-                  
                </div>
                </div>
                <div class="col-md-6 col-lg-6 offset-.5 mt-5" id="details">
                   <div class="card border-light shadow-none">
-                     <div class="pagination" id="pagination">
-                     <a href=""
-                        ><span
-                           class="iconify-inline"
-                           data-icon="ant-design:shopping-cart-outlined"
-                        ></span>
-                     </a>
+                     <div class="pagination mb-5" id="pagination">
+                     
                         <li>
                            <a href="item.html?product=${prev}" class="aPagination"
                               >❮ Prev</a
@@ -60,43 +70,60 @@ const displayData = (item) => {
                      <h2 class="text-dark">${item.name}</h2>
                      <p class="lead"></p>
                      <p class="lead fw-bold">$${item.price}</p>
-                     <p>Size</p>
-                     <select
+                     <p>Size</p><select
                         class="form-select"
+                        name="sizes"
+                        id="sizes"
                         aria-label="Default select example"
-                     >
-                        <option selected>Select</option>
-                        <option value="1">Large</option>
-                        <option value="2">Medium</option>
-                        <option value="3">Small</option>
-                        <option value="3">Extra Small</option>
-                     </select>
+                     >${options(item.id, item.size, "size")}</select>
                      <br />
                      <p>Color</p>
                      <select
                         class="form-select"
+                        name="colors"
+                        id="colors"
                         aria-label="Default select example"
-                     >
-                        <option selected>Select</option>
-                        <option value="1">Black</option>
-                        <option value="1">Dark Blue</option>
-                     </select>
-                     <br />
+                     >${options(item.id, item.color, "color")}</select><br />
                      <p class="text-dark">Quantity</p>
                      <div class="col-xs col-sm col-md col-lg-6 col-xl col-xxl">
                         <div class="input-group">
-                           <input
-                              type="number"
-                              step="1"
-                              max="10"
-                              value="1"
-                              name="quantity"
-                              class="quantity-field border text-center w-25"
-                           />
+                           <select name="quantity" id="quantity" class="quantity-field border text-center w-25">
+                           <option ${
+                             cart[item.id] && cart[item.id].qty == "1"
+                               ? "selected"
+                               : ""
+                           } value="1">1</option>
+                           <option ${
+                             cart[item.id] && cart[item.id].qty == "2"
+                               ? "selected"
+                               : ""
+                           } value="2">2</option>
+                           <option ${
+                             cart[item.id] && cart[item.id].qty == "3"
+                               ? "selected"
+                               : ""
+                           } value="3">3</option>
+                           <option ${
+                             cart[item.id] && cart[item.id].qty == "4"
+                               ? "selected"
+                               : ""
+                           } value="4">4</option>
+                           <option ${
+                             cart[item.id] && cart[item.id].qty == "5"
+                               ? "selected"
+                               : ""
+                           } value="5">5</option>
+                           </select>
                         </div>
                      </div>
                      <div class="col-md-4 col-sm-6 mt-4">
-                        <button id="btnAddToCart" class="btnAddToCart">
+                        <button id="btnAddToCart" data-id="${
+                          item.id
+                        }" data-img="${item.mainphoto}" data-price="${
+    item.price
+  }" data-item="${
+    item.name
+  }" onclick="javascript:addCart(event)" class="btnAddToCart">
                            Add to Cart
                         </button>
                      </div>
@@ -108,10 +135,7 @@ const displayData = (item) => {
             </div>
          </div>
                `;
-        
-      
-   
 
-   subContainer.innerHTML = htmlString;
+  subContainer.innerHTML = htmlString;
 };
 fetchData();
---
+++ order.js
@@ -0,0 +1,159 @@
+let count = 0;
+let sum = 0;
+let cart = {};
+
+if (localStorage.getItem("count")) {
+  count = parseInt(localStorage.getItem("count"));
+}
+
+if (localStorage.getItem("sum")) {
+  sum = parseInt(localStorage.getItem("sum"));
+}
+
+if (localStorage.getItem("cart")) {
+  cart = JSON.parse(localStorage.getItem("cart"));
+}
+
+const checkempt = function () {
+  if (!Object.keys(cart).length) {
+    alert("Cart is empty!");
+    location.href = "shop.html";
+  }
+};
+
+checkempt();
+
+let tbody = document.getElementById("tbody");
+let total_price = 0;
+let total_quantity = 0;
+
+for (let id in cart) {
+  let item = cart[id];
+
+  let tr = document.createElement("tr");
+
+  let item_td = document.createElement("td");
+  item_td.innerHTML = `<span class="item_img">
+    <a href="item.html?product=${item.id}">
+      <img src="${item.img}" />
+    </a>
+    </span>
+    <span class="item_item">
+      <a href="item.html?product=${item.id}">${item.item}</a>
+    </span>`;
+  tr.appendChild(item_td);
+
+  let price_td = document.createElement("td");
+  price_td.textContent = parseFloat(item.price) * parseInt(item.qty);
+  total_price += parseFloat(item.price) * parseInt(item.qty);
+  tr.appendChild(price_td);
+
+  tbody.appendChild(tr);
+}
+
+let table = document.getElementById("order");
+let footer = table.createTFoot();
+let row = footer.insertRow(0);
+let cell = row.insertCell(0);
+let cell2 = row.insertCell(1);
+
+let row1 = footer.insertRow(1);
+let cell11 = row1.insertCell(0);
+let cell22 = row1.insertCell(1);
+
+cell.innerHTML = `<span style="float:right;">Shipping costs</span>`;
+cell2.innerHTML = `<span id="shipping"></span>`;
+
+cell11.innerHTML = `<h5 style="text-align:right;"><strong>Subtotal</strong></h5>`;
+cell22.innerHTML = `<h4 id="total">$ ${total_price}</h4>`;
+
+const shippingfee = function (el) {
+  let selected = el.options[el.selectedIndex].value;
+  let standard_fee = 25;
+
+  if (selected == "PH") {
+    standard_fee = 10;
+  }
+  return standard_fee;
+};
+
+const calculateshipping = function (event) {
+  let shipping = document.getElementById("shipping");
+  let total = document.getElementById("total");
+
+  let standard_fee = shippingfee(this);
+
+  shipping.innerHTML = standard_fee;
+  total.innerHTML = total_price + standard_fee;
+};
+
+let updt = document.getElementById("country");
+updt.addEventListener("change", calculateshipping);
+
+const shippbilling = function () {
+  var billing_address = document.getElementById("billing_address");
+  billing_address.disabled = this.checked ? true : false;
+
+  if (!billing_address.disabled) {
+    billing_address.focus();
+  } else {
+    billing_address.value = "";
+  }
+};
+
+let shpbil = document.getElementById("same_shipping");
+shpbil.addEventListener("click", shippbilling);
+
+const placeOrder = function () {
+  let firstname = document.getElementById("firstname");
+  let lastname = document.getElementById("lastname");
+  let email_address = document.getElementById("email_address");
+  let country = document.getElementById("country");
+  let shipping_address = document.getElementById("shipping_address");
+  let same_shipping = document.getElementById("same_shipping");
+  let billing_address = document.getElementById("billing_address");
+  let same_shipp_billing = same_shipping.checked
+    ? "== Billing Same with Shipping =="
+    : billing_address.value;
+
+  let total_quantity_order = 0;
+  let item_item,
+    item_qty,
+    item_price,
+    format_order = "";
+
+  for (let id in cart) {
+    let item = cart[id];
+    item_item = item.item;
+    item_qty = item.qty;
+    item_price = parseFloat(item.price) * parseInt(item.qty);
+    format_order += `Item : ${item_item} | Quantity : ${item_qty} | Price : ${parseFloat(
+      item_price
+    )}\n`;
+    total_quantity_order += parseFloat(item.price) * parseInt(item.qty);
+  }
+  total_quantity_order += shippingfee(country);
+
+  let placeorder = `Name : ${firstname.value} \n Lastname : ${
+    lastname.value
+  } \n Email Address : ${email_address.value} \n\n Country : ${
+    country.options[country.selectedIndex].value
+  } \n Shipping Address : ${
+    shipping_address.value
+  } \n\n Billing Address : ${same_shipp_billing}\n\n Orders : \n ${format_order} \n Total Price : ${total_quantity_order}`;
+
+  Email.send({
+    SecureToken: "db232d4b-3842-4e61-951b-c611407d1e97",
+    To: email_address.value,
+    From: "josephlesterbacs@gmail.com",
+    Subject: "No Limit Orders",
+    Body: `<html>${placeorder}</html>`,
+  }).then(function (message) {
+    alert(placeorder);
+    localStorage.clear();
+    location.href = "index.html";
+  });
+};
+
+let sendorder = document.getElementById("placeorder");
+sendorder.addEventListener("click", placeOrder);
